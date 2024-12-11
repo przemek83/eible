afcdf78be591b5f0408b4ebb29afc43802bfee1c
Przemek <4788832+przemek83@users.noreply.github.com>
2024-12-11 20:53:16 +0100

    Build zlib always with release mode. Aplicable to MSVC.

--- ZlibConfig.cmake
+++ ZlibConfig.cmake
@@ -1,7 +1,7 @@
 configure_file(CMakeLists.txt.zlib zlib/CMakeLists.txt)
 execute_process(COMMAND ${CMAKE_COMMAND} -G ${CMAKE_GENERATOR} .
         WORKING_DIRECTORY ${CMAKE_CURRENT_BINARY_DIR}/zlib)
-execute_process(COMMAND ${CMAKE_COMMAND} --build .
+execute_process(COMMAND ${CMAKE_COMMAND} --build . --config Release
         WORKING_DIRECTORY ${CMAKE_CURRENT_BINARY_DIR}/zlib )
 
 set(ZLIB_ROOT ${CMAKE_CURRENT_BINARY_DIR}/zlib/install)
